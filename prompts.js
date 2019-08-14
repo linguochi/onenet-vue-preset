aefca9bf47208eb7349bfc407c1c34f6e7842f0f
lingc <382150914@qq.com>
2019-08-14 17:15:43 +0800

    add prompts.js

---
+++ prompts.js
@@ -0,0 +1,25 @@
+// 此 preset 在被初始化调用的时候，执行这个prompts
+// 被解析的答案对象会作为选项被传递给 generator。
+
+module.exports = [
+  {
+    name: 'ui-framework',
+    type: 'list',
+    message: 'choose UI Framework',
+    choices: [
+      {
+        name: 'none',
+        value: 'none'
+      },
+      {
+        name: 'Element UI',
+        value: 'element-ui'
+      },
+      {
+        name: 'iView',
+        value: 'iview'
+      }
+    ],
+    default: 'none'
+  }
+];
