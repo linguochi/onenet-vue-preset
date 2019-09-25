bb0876c739c4e6979c703954e6de2f839a4637fa
lingc <382150914@qq.com>
2019-09-25 16:25:13 +0800

    升级element-ui

--- ui/element/src/plugins/element.js
+++ ui/element/src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Element from "element-ui";
-import '~element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import "../style/element-diy.less";
 Vue.use(Element, { size: "small" });
 window.$loading = Vue.$loading;
