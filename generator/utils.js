4ae4e205ada36a4d65fb7b6f85a3a39c29b9a01f
lingc <382150914@qq.com>
2019-09-25 20:38:04 +0800

    add 两个常用工具函数

--- generator/utils.js
+++ generator/utils.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = api => {
   return {
@@ -25,8 +25,35 @@ module.exports = api => {
       }
       fs.rmdirSync(dir); //如果文件夹是空的，就将自己删除掉
     },
+    getMain() {
+      const tsPath = api.resolve('src/main.ts');
+      return fs.existsSync(tsPath) ? 'src/main.ts' : 'src/main.js';
+    },
+    updateBabelConfig(callback) {
+      let config, configPath;
+
+      const rcPath = api.resolve('./babel.config.js');
+      const pkgPath = api.resolve('./package.json');
+      if (fs.existsSync(rcPath)) {
+        configPath = rcPath;
+        config = callback(require(rcPath));
+      } else if (fs.existsSync(pkgPath)) {
+        configPath = pkgPath;
+        config = JSON.parse(fs.readFileSync(pkgPath, { encoding: 'utf8' }));
+        config.babel = callback(config.babel || {});
+      }
+
+      if (configPath) {
+        const moduleExports = configPath !== pkgPath ? 'module.exports = ' : '';
+        fs.writeFileSync(
+          configPath,
+          `${moduleExports}${JSON.stringify(config, null, 2)}`,
+          { encoding: 'utf8' }
+        );
+      }
+    },
     successTip() {
-      console.log("hello , 项目已成功安装，开始愉快编程吧");
+      console.log('hello , 项目已成功安装，开始愉快编程吧');
     }
   };
 };
