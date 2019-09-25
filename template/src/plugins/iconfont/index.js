38395c745852faf47eea3c946a09546839e47266
lingc <382150914@qq.com>
2019-09-25 15:12:36 +0800

    使用iconfont

---
+++ template/src/plugins/iconfont/index.js
@@ -0,0 +1,22 @@
+import './iconfont.js';
+
+import Icon from './IconFont.vue';
+
+let installed = false;
+
+const install = {
+  installed: false,
+  install(Vue, options = {}) {
+    if (this.installed) return;
+    this.installed = true;
+    Vue.component('icon', Icon);
+  },
+};
+
+Icon.install = install.install;
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(Icon);
+}
+
+export default Icon;
--- template/src/plugins/index.js
+++ template/src/plugins/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import axios from './axios';
+import './iconfont/index.js';
 
 Object.defineProperties(Vue.prototype, {
   $http: {
