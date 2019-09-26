d508e26d4e5dedfb85d594ea6090e3cbe44f85da
lingc <382150914@qq.com>
2019-09-26 08:45:17 +0800

    axios封装

--- template/src/api/user.js
+++ template/src/api/user.js
@@ -1,5 +1,6 @@
 export default {
-  getUser(params) {
-    return $axios.get('url_to_get', { params });
+  getUser(payload) {
+    let { url, params } = payload;
+    return $axios.get(url, { params });
   }
 };
